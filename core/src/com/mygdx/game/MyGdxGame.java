73d1b4278e678e22ba00899618ea3c4c6b1ca5f3
Ushal Naidoo <ushal.naidoo@nzdevushaln2.kiwiplan.co.nz>
2017-05-24 16:52:57 +1200

    Tried modifying the animation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,32 +1,23 @@
 package com.mygdx.game;
 
-import java.util.Iterator;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 
 	// Constant rows and columns of the sprite sheet
-	private static final int FRAME_COLS = 6, FRAME_ROWS = 1;
+	private static final int PERSON_FRAME_COLS = 6, PERSON_FRAME_ROWS = 1;
+	private static final int FRAME_COLS = 5, FRAME_ROWS = 4;
 
 	// Objects used
-	Animation<TextureRegion> walkAnimation; // Must declare frame type (TextureRegion)
-	Texture walkSheet;
+	Animation<TextureRegion> personAnimation; // Must declare frame type (TextureRegion)
+	Animation<TextureRegion> fireAnimation; // Must declare frame type (TextureRegion)
+	Texture personSheet, fireSheet;
 	SpriteBatch spriteBatch;
 
 	// A variable for tracking elapsed time for the animation
@@ -36,19 +27,37 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void create() {
 
 		// Load the sprite sheet as a Texture
-		walkSheet = new Texture(Gdx.files.internal("spritestrip.png"));
+		personSheet = new Texture(Gdx.files.internal("spritestrip.png"));
+		fireSheet = new Texture(Gdx.files.internal("fire.png"));
 
 		// Use the split utility method to create a 2D array of TextureRegions. This is
 		// possible because this sprite sheet contains frames of equal size and they are
 		// all aligned.
-		TextureRegion[][] tmp = TextureRegion.split(walkSheet,
-																								walkSheet.getWidth() / FRAME_COLS,
-																								walkSheet.getHeight() / FRAME_ROWS);
+		TextureRegion[][] tmp = TextureRegion.split(personSheet,
+																								personSheet.getWidth() / PERSON_FRAME_COLS,
+																								personSheet.getHeight() / PERSON_FRAME_ROWS);
 
 		// Place the regions into a 1D array in the correct order, starting from the top
 		// left, going across first. The Animation constructor requires a 1D array.
-		TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+		TextureRegion[] walkFrames = new TextureRegion[PERSON_FRAME_COLS * PERSON_FRAME_ROWS];
 		int index = 0;
+		for (int i = 0; i < PERSON_FRAME_ROWS; i++) {
+			for (int j = 0; j < PERSON_FRAME_COLS; j++) {
+				walkFrames[index++] = tmp[i][j];
+			}
+		}
+
+		// Initialize the Animation with the frame interval and array of frames
+		personAnimation = new Animation<TextureRegion>(0.08f, walkFrames);
+
+		tmp = TextureRegion.split(fireSheet,
+															fireSheet.getWidth() / FRAME_COLS,
+															fireSheet.getHeight() / FRAME_ROWS);
+
+		// Place the regions into a 1D array in the correct order, starting from the top
+		// left, going across first. The Animation constructor requires a 1D array.
+		walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+		index = 0;
 		for (int i = 0; i < FRAME_ROWS; i++) {
 			for (int j = 0; j < FRAME_COLS; j++) {
 				walkFrames[index++] = tmp[i][j];
@@ -56,7 +65,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		}
 
 		// Initialize the Animation with the frame interval and array of frames
-		walkAnimation = new Animation<TextureRegion>(0.08f, walkFrames);
+		fireAnimation = new Animation<TextureRegion>(0.08f, walkFrames);
+
 
 		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
 		// time to 0
@@ -70,8 +80,12 @@ public class MyGdxGame extends ApplicationAdapter {
 		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
 
 		// Get current frame of animation for the current stateTime
-		TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
+		TextureRegion currentFrame = personAnimation.getKeyFrame(stateTime, true);
 		spriteBatch.begin();
+		// process user input
+		if(Gdx.input.isTouched()) {
+			currentFrame = fireAnimation.getKeyFrame(stateTime, false);
+		}
 		spriteBatch.draw(currentFrame, 50, 50); // Draw current frame at (50, 50)
 		spriteBatch.end();
 	}
@@ -79,6 +93,6 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void dispose() { // SpriteBatches and Textures must always be disposed
 		spriteBatch.dispose();
-		walkSheet.dispose();
+		personSheet.dispose();
 	}
 }
